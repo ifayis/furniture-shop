e8c994d9054e154592691d70ac7bb39ff80be07e
Fayis KV <mohammedfayis135@gmail.com>
2026-01-31 13:53:51 +0530

    implemented Api for the categories based on id

--- src/api/categoryApi.js
+++ src/api/categoryApi.js
@@ -4,3 +4,8 @@ export const getAllCategories = async () => {
   const response = await api.get("/api/categories/all");
   return response.data.data;
 };
+
+export const getCategoryById = async (id) => {
+  const response = await api.get(`/api/categories/${id}`);
+  return response.data.data;
+};
